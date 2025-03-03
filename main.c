e4f0e26765789ef7656fd716a5a3ec892a49d3b1
AbdelrahmanWaelH <abdelrahmanw20@gmail.com>
2025-03-03 14:27:08 +0200

    added main file with threads 1 & 3

---
+++ main.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <pthread.h>
+
+void inBetweenChars(){ //Thread 1 start
+    char start;
+    char end;
+    printf("Enter the start character: ");
+    scanf(" %c",&start);
+    printf("Enter the end character: ");
+    scanf(" %c", &end);
+    for (; start <= end; start++){
+        printf("%c\n",start);
+    }
+} //Thread 1 end
+
+void sumAvgProduct(){ //Thread 3 start
+    int start;
+    int end;
+    int sum = 0;
+    int product = 1;
+    float average = 0;
+    printf("Enter the start integer: ");
+    scanf(" %d", &start);
+    printf("Enter the end integer: ");
+    scanf(" %d", &end);
+    int delta = end - start;
+    for (; start <= end; start++){
+        sum += start;
+        product *= start;
+    }
+    average = (float) sum/delta;
+    printf("\nThe sum is : %d", sum);
+    printf("\nThe product is : %d", product);
+    printf("\nThe average is : %f\n", average);
+} //Thread 3 end 
+
+
+
+int main(){
+    inBetweenChars();
+    sumAvgProduct();
+    return 0;
+}
