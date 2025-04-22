6b3732255ad8c9a13c3183620dd1068f5502cb83
OmarTarekFahmy <omarico593@gmail.com>
2025-04-22 16:26:14 +0200

    Completed Single Line PCB Execution

--- Milestone2/src/main.c
+++ Milestone2/src/main.c
@@ -38,8 +38,8 @@ typedef struct {
 
 typedef struct {
 
-    char* name;
-    char* data;
+    char name[64];
+    char data[64];
 
 } MemoryWord;
 
@@ -112,6 +112,8 @@ sem_t fileSemaphore;
 int processIdCounter = 0;
 
 Memory mainMemory;
+ReadyQueue readyQueue;
+BlockedQueue blockedQueue;
 
 char* filepathA =  "Program_1.txt";
 char* filepathB = "";
@@ -122,11 +124,16 @@ int main(){
     // Prompt user for scheduling algo.
     initMemory();
     ProcessControlBlock pcbA = loadProcess(filepathA);
+
+    while(pcbA.program_counter + CODE_SEGMENT_OFFSET < pcbA.memory_end){
+        executeSingleLinePCB(&pcbA);
+    }
+
     displayMemory(&mainMemory);
 
-    executeSingleLinePCB(&pcbA);
 
-    displayMemory(&mainMemory);
+
+    //displayMemory(&mainMemory);
 
     // loadProcess(filepathB);
     // loadProcess(filepathC);
@@ -146,7 +153,6 @@ int main(){
     //     break;
     // }
 
-    
 
 
     
@@ -168,15 +174,16 @@ void initSemaphores(){
 
 void executeSingleLinePCB(ProcessControlBlock* processControlBlock){
 
+    
     int memoryStart = processControlBlock->memory_start;
     int currentLine = memoryStart + processControlBlock->program_counter + CODE_SEGMENT_OFFSET;
-
+    
     char* line = mainMemory.memoryArray[currentLine].data;
     char** tokens = lineParser(line);
     
     /*
         Tokens:
-            assign
+            assign DONE
             semWait
             semSignal
             print
@@ -195,32 +202,48 @@ void executeSingleLinePCB(ProcessControlBlock* processControlBlock){
             */
 
             char* filepath = tokens[3];
+            //removeNewline(filepath);
+            filepath[strcspn(filepath, "\r\n")] = '\0';
             char* fileString = readFile(filepath);
 
             if(strcmp(target, "a") == 0){
-                mainMemory.memoryArray[processControlBlock->memory_start + 6].data = fileString;
+                //mainMemory.memoryArray[processControlBlock->memory_start + 6].data = fileString;
+                strcpy(mainMemory.memoryArray[processControlBlock->memory_start + 6].data, fileString);
             }else if (strcmp(target, "b") == 0){
-                mainMemory.memoryArray[processControlBlock->memory_start + 7].data = fileString;
+                //mainMemory.memoryArray[processControlBlock->memory_start + 7].data = fileString;
+                strcpy(mainMemory.memoryArray[processControlBlock->memory_start + 7].data, fileString);
             }else {
-                mainMemory.memoryArray[processControlBlock->memory_start + 8].data = fileString;
+                //mainMemory.memoryArray[processControlBlock->memory_start + 8].data = fileString;
+                strcpy(mainMemory.memoryArray[processControlBlock->memory_start + 8].data, fileString);
             }
 
-        }else if (strcmp(value, "input") == 0){
+        }else if (strcmp(value, "input\n") == 0){
 
             /*
             Handle Case for 
                 assign a input
             */
-            char* userInputValue;
+
+            char userInputValue[64];
             printf("Please enter a value:\n");
-            scanf("%s", userInputValue);
+            //scanf("%s", userInputValue);
+            char* buffer = malloc(64);
+            fgets(buffer, 64, stdin);
+            buffer[strcspn(buffer, "\r\n")] = '\0';
+            strcpy(userInputValue, buffer);
+
+            
 
             if(strcmp(target, "a") == 0){
-                mainMemory.memoryArray[processControlBlock->memory_start + 6].data = userInputValue;
+                //mainMemory.memoryArray[processControlBlock->memory_start + 6].data = userInputValue;
+                strcpy(mainMemory.memoryArray[processControlBlock->memory_start + 6].data, userInputValue);
+                
             }else if (strcmp(target, "b") == 0){
-                mainMemory.memoryArray[processControlBlock->memory_start + 7].data = userInputValue;
+                //mainMemory.memoryArray[processControlBlock->memory_start + 7].data = userInputValue;
+                strcpy(mainMemory.memoryArray[processControlBlock->memory_start + 7].data, userInputValue);
             }else {
-                mainMemory.memoryArray[processControlBlock->memory_start + 8].data = userInputValue;
+                //mainMemory.memoryArray[processControlBlock->memory_start + 8].data = userInputValue;
+                strcpy(mainMemory.memoryArray[processControlBlock->memory_start + 8].data, userInputValue);
             }
 
             
@@ -230,13 +253,22 @@ void executeSingleLinePCB(ProcessControlBlock* processControlBlock){
                 assign a b
             */
 
-            if(strcmp(target, "a") == 0){
-                mainMemory.memoryArray[processControlBlock->memory_start + 6].data = value;
-            }else if (strcmp(target, "b") == 0){
-                mainMemory.memoryArray[processControlBlock->memory_start + 7].data = value;
-            }else {
-                mainMemory.memoryArray[processControlBlock->memory_start + 8].data = value;
+            int indexOfTarget = 0;
+            int indexOfValue = 0;
+            for(int i = 6; i <=8; i++){
+                if(strcmp(mainMemory.memoryArray[processControlBlock->memory_start + i].name, target) == 0){
+                    indexOfTarget = i;
+                }
+                if(strcmp(mainMemory.memoryArray[processControlBlock->memory_start + i].name, value) == 0){
+                    indexOfValue = i;
+                }
             }
+
+            
+            //mainMemory.memoryArray[processControlBlock->memory_start + indexOfTarget].data = mainMemory.memoryArray[processControlBlock->memory_start + indexOfValue].data;
+            strcpy(mainMemory.memoryArray[processControlBlock->memory_start + indexOfTarget].data, mainMemory.memoryArray[processControlBlock->memory_start + indexOfValue].data);
+
+
 
         }
 
@@ -244,31 +276,114 @@ void executeSingleLinePCB(ProcessControlBlock* processControlBlock){
         
     }
     else if (strcmp(tokens[0], "print") == 0) {
-        char* var = tokens[1];
-        // TODO: handle print
+        char* value = tokens[1];
+        int indexOfValue = 0;
+        char* data;
+        for(int i = 6; i <=8; i++){
+            if(strcmp(mainMemory.memoryArray[processControlBlock->memory_start + i].name, value) == 0){
+                data = mainMemory.memoryArray[processControlBlock->memory_start + i].data;
+            }
+        }
+        printf("%s\n", data);
     }
     else if (strcmp(tokens[0], "printFromTo") == 0) {
         char* start = tokens[1];
         char* end = tokens[2];
-        // TODO: handle printFromTo
+
+        end[strcspn(end, "\r\n")] = '\0';
+
+        int indexOfValue = 0;
+        char* dataStart;
+        char* dataEnd;
+
+        //displayMemory(&mainMemory);
+
+        for(int i = 6; i <=8; i++){
+            if(strcmp(mainMemory.memoryArray[processControlBlock->memory_start + i].name, start) == 0){
+                dataStart = mainMemory.memoryArray[processControlBlock->memory_start + i].data;
+            }
+
+        }
+        for(int i = 6; i <= 8; i++){
+            
+            if(strcmp(mainMemory.memoryArray[processControlBlock->memory_start + i].name, end) == 0){
+                dataEnd = mainMemory.memoryArray[processControlBlock->memory_start + i].data;
+            }
+        }
+        
+        int startInt = atoi(dataStart);
+        int endInt = atoi(dataEnd);
+        
+        for(int i = startInt; i <= endInt; i++){
+            printf("%d ", i);
+        }
+        printf("\n");
+
     }
     else if (strcmp(tokens[0], "semWait") == 0) {
         char* resource = tokens[1];
-        // TODO: handle semWait
+        int semValue;
+
+        if(strcmp(resource, "file") == 0){
+
+            sem_getvalue(&fileSemaphore, &semValue);
+            if(semValue == 0) {
+                Q_enqueue(&(blockedQueue.blockedQueue), processControlBlock->id);
+            }else{
+                safe_sem_wait(&fileSemaphore);
+            }
+
+        }else if (strcmp(resource, "userInput")){
+            sem_getvalue(&userInputSemaphore, &semValue);
+            if(semValue == 0) {
+                Q_enqueue(&(blockedQueue.blockedQueue), processControlBlock->id);
+            }else{
+                safe_sem_wait(&userInputSemaphore);
+            }
+        }else if (strcmp(resource, "userOutput")){
+            sem_getvalue(&userOutputSemaphore, &semValue);
+            if(semValue == 0) {
+                Q_enqueue(&(blockedQueue.blockedQueue), processControlBlock->id);
+            }else{
+                safe_sem_wait(&userOutputSemaphore);
+            }
+        }
+        
     }
     else if (strcmp(tokens[0], "semSignal") == 0) {
         char* resource = tokens[1];
-        // TODO: handle semSignal
+        if(strcmp(resource, "file") == 0){
+            safe_sem_post(&fileSemaphore);
+        }else if (strcmp(resource, "userInput")){
+            safe_sem_post(&userInputSemaphore);
+        }else if (strcmp(resource, "userOutput")){
+            safe_sem_post(&userOutputSemaphore);
+        }
+        
     }
     else if (strcmp(tokens[0], "writeFile") == 0) {
         char* filenameVar = tokens[1];
         char* dataVar = tokens[2];
-        // TODO: handle writeFile
+        dataVar[strcspn(dataVar, "\r\n")] = '\0';
+
+        char* dataToWrite;
+        char* filename;
+    
+        
+        for(int i = 6; i <=8; i++){
+            if(strcmp(mainMemory.memoryArray[processControlBlock->memory_start + i].name, dataVar) == 0){
+                dataToWrite = mainMemory.memoryArray[processControlBlock->memory_start + i].data;
+            }
+            if(strcmp(mainMemory.memoryArray[processControlBlock->memory_start + i].name, filenameVar) == 0){
+                filename = mainMemory.memoryArray[processControlBlock->memory_start + i].data;
+            }
+            
+        }
+        FILE* newFile = fopen(filename, "w");
+        fprintf(newFile, "%s", dataToWrite);
+
     }
-    else if (strcmp(tokens[0], "readFile") == 0) {
-        char* filenameVar = tokens[1];
-        // TODO: handle readFile
-    }
+
     else {
         // Unknown instruction
         printf("Unknown instruction: %s\n", tokens[0]);
@@ -285,9 +400,11 @@ void executeSingleLinePCB(ProcessControlBlock* processControlBlock){
 char** lineParser(char* line){
 
     char** tokenArray = (char**)malloc(sizeof(char*) * 16); //Assuming maximum of 8 tokens per line.
-    char* token = strtok(line, " ");
+
     char lineCopy[strlen(line)];
     strcpy(lineCopy, line);
+    char* token = strtok(lineCopy, " ");
+
 
 
     int counter = 0;
@@ -298,6 +415,7 @@ char** lineParser(char* line){
 
     }
     tokenArray[counter] = NULL;
+
 
     return tokenArray;
 
@@ -329,13 +447,11 @@ void safe_sem_wait(sem_t* __sem){
 
 void initMemory() {
     for (int i = 0; i < 60; i++) {
-        // Allocate memory for name and data
-        mainMemory.memoryArray[i].name = malloc(64 * sizeof(char)); // or any size you need
-        mainMemory.memoryArray[i].data = malloc(64 * sizeof(char)); // or any size you need
+        
+        //mainMemory.memoryArray[i].name = malloc(64 * sizeof(char)); // or any size you need
+        //mainMemory.memoryArray[i].data = malloc(64 * sizeof(char)); // or any size you need
 
-        // Initialize name and data
-        mainMemory.memoryArray[i].name[0] = '\0'; // Empty string initialization
-        mainMemory.memoryArray[i].data[0] = '\0'; // Empty string initialization
+        
     }
 }
 
@@ -347,6 +463,11 @@ ProcessControlBlock loadProcess(char* filepath){
     int indexOfFree = 0;
     while(strcmp(mainMemory.memoryArray[indexOfFree].name, "")){
         indexOfFree++;
+    }
+
+    if(indexOfFree > 40){
+        perror("Not enough memory to store process in memory");
+        return pcb;
     }
 
     pcb.id = processIdCounter;
@@ -361,35 +482,44 @@ ProcessControlBlock loadProcess(char* filepath){
 
     pcb.state = READY;
 
-    char buffer[64];
+    char* buffer = malloc(64);
 
-    mainMemory.memoryArray[pcb.memory_start + 0].name = "pid";
+    //mainMemory.memoryArray[pcb.memory_start + 0].name = "pid";
+    strcpy(mainMemory.memoryArray[pcb.memory_start + 0].name, "pid");
     sprintf(buffer, "%d", pcb.id);
     strcpy(mainMemory.memoryArray[pcb.memory_start + 0].data, buffer);
-
-    mainMemory.memoryArray[pcb.memory_start + 1].name = "mem_start";
+    
+    //mainMemory.memoryArray[pcb.memory_start + 1].name = "mem_start";
+    strcpy(mainMemory.memoryArray[pcb.memory_start + 1].name, "mem_start");
     sprintf(buffer, "%d", pcb.memory_start);
     strcpy(mainMemory.memoryArray[pcb.memory_start + 1].data, buffer);
-
-    mainMemory.memoryArray[pcb.memory_start + 2].name = "mem_end";
+    
+    //mainMemory.memoryArray[pcb.memory_start + 2].name = "mem_end";
+    strcpy(mainMemory.memoryArray[pcb.memory_start + 2].name, "mem_end");
     sprintf(buffer, "%d", pcb.memory_end);
     strcpy(mainMemory.memoryArray[pcb.memory_start + 2].data, buffer);
-
-    mainMemory.memoryArray[pcb.memory_start + 3].name = "priority";
+    
+    //mainMemory.memoryArray[pcb.memory_start + 3].name = "priority";
+    strcpy(mainMemory.memoryArray[pcb.memory_start + 3].name, "priority");
     sprintf(buffer, "%d", pcb.priority);
     strcpy(mainMemory.memoryArray[pcb.memory_start + 3].data, buffer);
-
-    mainMemory.memoryArray[pcb.memory_start + 4].name = "program_counter";
+    
+    //mainMemory.memoryArray[pcb.memory_start + 4].name = "program_counter";
+    strcpy(mainMemory.memoryArray[pcb.memory_start + 4].name, "program_counter");
     sprintf(buffer, "%d", pcb.program_counter);
     strcpy(mainMemory.memoryArray[pcb.memory_start + 4].data, buffer);
-
-    mainMemory.memoryArray[pcb.memory_start + 5].name = "state";
+    
+    //mainMemory.memoryArray[pcb.memory_start + 5].name = "state";
+    strcpy(mainMemory.memoryArray[pcb.memory_start + 5].name, "state");
     sprintf(buffer, "%s", processToString(pcb.state));
     strcpy(mainMemory.memoryArray[pcb.memory_start + 5].data, buffer);
-
-    mainMemory.memoryArray[pcb.memory_start + 6].name = "a";
-    mainMemory.memoryArray[pcb.memory_start + 7].name = "b";
-    mainMemory.memoryArray[pcb.memory_start + 8].name = "c";
+    
+    strcpy(mainMemory.memoryArray[pcb.memory_start + 6].name, "a");
+    strcpy(mainMemory.memoryArray[pcb.memory_start + 7].name, "b");
+    strcpy(mainMemory.memoryArray[pcb.memory_start + 8].name, "c");
+    // mainMemory.memoryArray[pcb.memory_start + 6].name = "a";
+    // mainMemory.memoryArray[pcb.memory_start + 7].name = "b";
+    // mainMemory.memoryArray[pcb.memory_start + 8].name = "c";
 
     FILE* code_file = fopen(filepath, "r");
 
@@ -399,12 +529,21 @@ ProcessControlBlock loadProcess(char* filepath){
     
     
     int code_segment_line = 0;
+    
     while(fgets(buffer, 64, code_file)){
         
-        mainMemory.memoryArray[pcb.memory_start + CODE_SEGMENT_OFFSET + code_segment_line].name = "code";
-        strcpy(mainMemory.memoryArray[pcb.memory_start + CODE_SEGMENT_OFFSET + code_segment_line].data, buffer);
+        strcpy(mainMemory.memoryArray[pcb.memory_start + CODE_SEGMENT_OFFSET + code_segment_line].name, "code");
+        //memcpy(mainMemory.memoryArray[pcb.memory_start + CODE_SEGMENT_OFFSET + code_segment_line].data, buffer, 64);
+        //printf("Buffer: %c\n", buffer[7]);
+
+        for(int i = 0; i < 64; i++){
+            mainMemory.memoryArray[pcb.memory_start + CODE_SEGMENT_OFFSET + code_segment_line].data[i] = buffer[i];
+        }
+        
         code_segment_line++;
     }
+    code_segment_line--;
+    pcb.memory_end = pcb.memory_start + CODE_SEGMENT_OFFSET + code_segment_line;
 
     return pcb;
 
